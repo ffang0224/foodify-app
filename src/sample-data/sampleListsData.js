c80bf4fa9e20709baaf79e9b318df3044df4c7b8
ffang0224 <157495940+ffang0224@users.noreply.github.com>
2024-11-07 14:42:48 -0500

    Sample data modified

--- src/sample-data/sampleListsData.js
+++ src/sample-data/sampleListsData.js
@@ -1,30 +1,33 @@
 const sampleListsData = [
-    {
-        author: "Bob Williams",
-        image:"https://brooklynbrewery.com/wp-content/uploads/2020/01/IQY_3198-scaled.jpg",
-        description: "Relaxing Chinese tea house music",
-        name: "Chinese Tea House",
-        restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
-        username: "bob_williams"
-    },
-    
-    {
-        author: "John Doe",
-        image: "https://fnb.qdc.vn/pictures/catalog/pasta-paradise/thiet-ke-nha-hang-pasta-paradise-04.jpg",
-        description: "Large group gatherings",
-        name: "Big Party",
-        restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
-        username: "john_doe"
-    },
+  {
+    author: "Bob Williams",
+    image:
+      "https://brooklynbrewery.com/wp-content/uploads/2020/01/IQY_3198-scaled.jpg",
+    description: "Best Chinese food in town!",
+    name: "Chinese Good Eats",
+    restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
+    username: "bob_williams",
+  },
 
-    {
-        author: "William Smith",
-        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/d0/62/eb/the-spice-route.jpg?w=1200&h=-1&s=1",
-        description: "Romantic date locations",
-        name: "Date places",
-        restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
-        username: "william_smith"
-    },
-]
+  {
+    author: "John Doe",
+    image:
+      "https://fnb.qdc.vn/pictures/catalog/pasta-paradise/thiet-ke-nha-hang-pasta-paradise-04.jpg",
+    description: "Restaurants for large group gatherings.",
+    name: "Group Hangouts",
+    restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
+    username: "john_doe",
+  },
+
+  {
+    author: "William Smith",
+    image:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/d0/62/eb/the-spice-route.jpg?w=1200&h=-1&s=1",
+    description: "Wanna impress your date? Check these places out!",
+    name: "Date places",
+    restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
+    username: "william_smith",
+  },
+];
 
 export default sampleListsData;

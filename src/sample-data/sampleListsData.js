a1675885d2412a18dd8488ea0cfc2e3beddd1846
aruzhan-bolatova <ab10028@nyu.edu>
2024-11-07 20:48:07 -0500

    Update sampleListsData

--- src/sample-data/sampleListsData.js
+++ src/sample-data/sampleListsData.js
@@ -2,7 +2,7 @@ const sampleListsData = [
   {
     author: "Bob Williams",
     image:
-      "https://brooklynbrewery.com/wp-content/uploads/2020/01/IQY_3198-scaled.jpg",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/df/b1/ae/chynna.jpg?w=1400&h=800&s=1",
     description: "Best Chinese food in town!",
     name: "Chinese Good Eats",
     restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
@@ -12,7 +12,7 @@ const sampleListsData = [
   {
     author: "John Doe",
     image:
-      "https://fnb.qdc.vn/pictures/catalog/pasta-paradise/thiet-ke-nha-hang-pasta-paradise-04.jpg",
+      "https://images.suitcasemag.com/wp-content/uploads/2023/10/10203432/the-eight-best-new-york-restaurants-for-a-party_652d5b8b3d678.jpeg",
     description: "Restaurants for large group gatherings.",
     name: "Group Hangouts",
     restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
@@ -24,7 +24,7 @@ const sampleListsData = [
     image:
       "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/13/d0/62/eb/the-spice-route.jpg?w=1200&h=-1&s=1",
     description: "Wanna impress your date? Check these places out!",
-    name: "Date places",
+    name: "For Date Nights",
     restaurants: ["/restaurants/SPICE001", "/restaurants/SPICE002"],
     username: "william_smith",
   },

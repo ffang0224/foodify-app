f5275c349605e38f5c7c1744b24f711089f9a008
elyaziaa <ea2749@nyu.edu>
2024-12-08 13:01:00 -0500

    Create ThemeToggle.js

---
+++ src/ThemeToggle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from '../contexts/ThemeContext';
+
+const ThemeToggle = () => {
+  const { isDarkMode, toggleTheme } = useTheme();
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-lg transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+    >
+      {isDarkMode ? (
+        <Sun className="w-5 h-5 text-yellow-500" />
+      ) : (
+        <Moon className="w-5 h-5 text-gray-600" />
+      )}
+    </button>
+  );
+};
+
+export default ThemeToggle;
